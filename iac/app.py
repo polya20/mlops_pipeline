13303ad9ec7a1ca8ebca257f6bf156d5cb1bde51
polya20 <subhojit20@gmail.com>
2025-08-11 21:54:50 +0200

    "feat(cdk): Use qualified bootstrap synthesizer"

--- iac/app.py
+++ iac/app.py
@@ -1,7 +1,22 @@
+# iac/app.py
 #!/usr/bin/env python3
 import aws_cdk as cdk
 from iac_stack import JackpotOptimizerStack
 
 app = cdk.App()
-JackpotOptimizerStack(app, "JackpotOptimizerStack")
+
+# --- START OF FIX ---
+# Define the synthesizer with the qualifier
+# This tells the CDK to use the roles and resources from the qualified bootstrap stack
+qualifier_synthesizer = cdk.DefaultStackSynthesizer(
+    qualifier="hnb659fds"
+)
+
+JackpotOptimizerStack(
+    app, 
+    "JackpotOptimizerStack",
+    synthesizer=qualifier_synthesizer # <-- Pass the synthesizer to the stack
+)
+# --- END OF FIX ---
+
 app.synth()
